6dc709b259c2744c1b9675644322c83fe09a17d7
stxrna <eric.starnini@gmail.com>
2023-02-09 10:17:48 +0100

    Aggiunto componenti

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PersonalInfoComponent } from './home/personal-info/personal-info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path:'personal-info', component:PersonalInfoComponent}  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
