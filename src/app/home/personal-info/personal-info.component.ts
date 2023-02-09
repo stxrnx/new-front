e91e7b71e2c041b0c8725cb0b20fea9b0e40ed8d
stxrna <eric.starnini@gmail.com>
2023-02-09 16:57:29 +0100

    messo a posto la form

--- src/app/home/personal-info/personal-info.component.ts
+++ src/app/home/personal-info/personal-info.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-personal-info',
   templateUrl: './personal-info.component.html',
   styleUrls: ['./personal-info.component.css']
 })
-export class PersonalInfoComponent {
+export class PersonalInfoComponent implements OnInit {
 
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
 }
